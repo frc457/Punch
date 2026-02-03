27d77c6c8e50b35ed11f9ca752cc2cd0a74e3e25
JaydenM457 <jalexmartinez0572@gmailcom>
2026-02-02 18:51:24 -0600

    first commit

---
+++ src/main/java/frc/robot/commands/CommandTrain.java
@@ -0,0 +1,102 @@
+package frc.robot.commands;
+
+
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.RPM;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.GateSubsystem;
+import frc.robot.subsystems.IndexerSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.TransportSubsytem;
+
+
+public class CommandTrain {
+    
+    private ArmSubsystem m_arm;
+    private GateSubsystem m_gate;
+    private IndexerSubsystem m_indexer;
+    private IntakeSubsystem m_intake;
+    private ShooterSubsystem m_shooter;
+    private TransportSubsytem m_transport;
+
+    public CommandTrain(ArmSubsystem Arm, GateSubsystem Gate, IndexerSubsystem Indexer, 
+                      IntakeSubsystem Intake, ShooterSubsystem Shooter, TransportSubsytem Transport){
+        m_arm = Arm;
+        m_gate = Gate;
+        m_indexer = Indexer;
+        m_intake = Intake;
+        m_shooter = Shooter;
+        m_transport = Transport;
+
+    }
+
+
+    public Command shoot(){ 
+        return m_shooter.setVelocity(RPM.of(500))
+        .alongWith(m_transport.setVelocity(RPM.of(500))
+        .withTimeout(1)) 
+        .andThen(
+            m_intake.setVelocity(RPM.of(50)) 
+            .alongWith(m_indexer.setVelocity(RPM.of(50))) 
+            .alongWith(m_gate.setVelocity(RPM.of(50))) 
+            .alongWith(m_shooter.setVelocity(RPM.of(500))) 
+            .alongWith(m_transport.setVelocity(RPM.of(500))) 
+            .alongWith(m_arm.setAngle(Degrees.of(60)))
+        );
+    }
+
+
+    public Command Intaking(){
+        return m_arm.setAngle(Degrees.of(60))
+        .alongWith(m_intake.setVelocity(RPM.of(100)))
+        .alongWith(m_indexer.setVelocity(RPM.of(100)));
+    }
+
+
+    //Pathplanner Commands
+
+    public Command timedShoot(){
+        return m_shooter.setVelocity(RPM.of(500))
+        .alongWith(m_transport.setVelocity(RPM.of(500))
+        .withTimeout(1)) 
+        .andThen(
+            m_intake.setVelocity(RPM.of(50)) 
+            .alongWith(m_indexer.setVelocity(RPM.of(50))) 
+            .alongWith(m_gate.setVelocity(RPM.of(50))) 
+            .alongWith(m_shooter.setVelocity(RPM.of(500))) 
+            .alongWith(m_transport.setVelocity(RPM.of(500))) 
+            .alongWith(m_arm.setAngle(Degrees.of(60)))
+            .withTimeout(4)
+        );
+    }
+
+
+    public Command timedIntaking(){
+        return m_arm.setAngle(Degrees.of(60))
+        .alongWith(m_intake.setVelocity(RPM.of(100)))
+        .alongWith(m_indexer.setVelocity(RPM.of(10)))
+        .withTimeout(2);
+    }
+
+
+    public Command mixer(){
+        return m_indexer.setVelocity(RPM.of(200)).withTimeout(0.5)
+        .andThen(m_indexer.setVelocity(RPM.of(-200)).withTimeout(0.5))
+        .andThen(m_indexer.setVelocity(RPM.of(200)).withTimeout(0.5));
+    }
+    
+
+    public Command throwup(){
+        return m_arm.setAngle(Degrees.of(60))
+        .andThen(m_intake.setVelocity(RPM.of(-50)), 
+            m_indexer.setVelocity(RPM.of(-50)),
+            m_gate.setVelocity(RPM.of(-50)), 
+            m_shooter.setVelocity(RPM.of(-500)), 
+            m_transport.setVelocity(RPM.of(-500)));
+    }
+
+
+}
